506945769a76a7065ee6643d46b4bcb7a41ca7eb
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-08-24 16:12:47 +0200

    some changes made

--- index.php
+++ index.php
@@ -23,34 +23,33 @@
     </div>
     <div class="menu">
                         <ul>
-                            <li><a class="active" href="/index.php">HOME</a></li>
-                            <li>
-                                <a id="products" href="#" >OUR PRODUCTS</a>
-                                <ul id="drop_list">
-                                    <optgroup label="Non-Alcoholic Drinks" >
-                                        <li><a href="#">Water</a></li>
-                                        <li><a href="#">Milk</a></li>
-                                        <li><a href="#">Carbonated Drinks</a></li>
-                                        <li><a href="#">Juice and plent drinks</a></li>
-                                    </optgroup>
-                                    <optgroup label="Alcoholic Drinks" >
-                                        <li><a href="#">Beers</a></li>
-                                        <li><a href="#">Ciders</a></li>
-                                        <li><a href="#">Wines</a></li>                        
-                                        <li><a href="#">Spirits</a></li>    
-                                    </optgroup>
-                                </ul>
-                            </li>
+                                <li><a class="un_drop" href="/index.php">HOME</a></li>
+                                <div class="drop" >
+                                <li> <a id="products" >OUR PRODUCTS</a>
+                                    <ul id="drop_list">
+                                            <li><a href="#">Water</a></li>
+                                            <li><a href="#">Milk</a></li>
+                                            <li><a href="#">Carbonated Drinks</a></li>
+                                            <li><a href="#">Juice and plent drinks</a></li>
+                                            <li><a href="#">Beers</a></li>
+                                            <li><a href="#">Ciders</a></li>
+                                            <li><a href="#">Wines</a></li>
+                                            <li><a href="#">Spirits</a></li>
+                                    </ul>
+                                </li>
+                            </div>
                             <!--<a href="/Links/clients.html">CLIENTS</a> -->
-                            <li><a href="Links/vendor.php">VENDORS</a></li>
-                            <li><a href="Links/owner.php">OWNER</a></li>
-                            <li><a href="Links/about.php">ABOUT US</a></li>
-                            <li><a href="Links/contacts.php">CONTACT US</a></li>
+                            <div class="un_drop">
+                                <li><a href="Links/vendor.php">VENDORS</a></li>
+                                <li><a href="Links/owner.php">OWNER</a></li>
+                                <li><a href="Links/about.php">ABOUT US</a></li>
+                                <li><a href="Links/contacts.php">CONTACT US</a></li>
+                            </div>
                         </ul>
     </div>
     </div>
 </div class="footer">
-   <p>heloo</p>
+   <p></p>
 <div>
 </body>
 
