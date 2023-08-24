451be5a06ec11251cb578778d3be8f9d2ad9800d
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-08-24 12:22:48 +0200

    created new file dashboard

---
+++ Links/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="CSS/styles.css">
+    <meta charset="">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <div style="display:flex;">
+        <h1>Admin Dashboard</h1>
+    </div>
+    <div>
+        <img style="float:left" src="../pictures/logo.PNG" alt="our logo">
+    </div>
+</body>
+
+</html>
+
