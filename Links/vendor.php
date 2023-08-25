798583aec13e6f20275ca933def5846a8bf4ae53
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-08-25 14:07:23 +0200

    change in links and styles

--- Links/vendor.php
+++ Links/vendor.php
@@ -4,9 +4,37 @@
     <link rel="stylesheet" href="../CSS/styles.css" />
   </head>
   <body>
-    <?php
-      include "nav.php";
-    ?>
+   <div class="container">
+    <div class="logo">
+        <img src="pictures/logo.PNG" width="100px" alt="">
+    </div>
+    <div class="menu">
+                        <ul>
+                                <li><a class="un_drop" href="../index.php">HOME</a></li>
+                                <div class="drop" >
+                                <li> <a id="products" >OUR PRODUCTS</a>
+                                    <ul id="drop_list">
+                                            <li><a href="#">Water</a></li>
+                                            <li><a href="#">Milk</a></li>
+                                            <li><a href="#">Carbonated Drinks</a></li>
+                                            <li><a href="#">Juice and plent drinks</a></li>
+                                            <li><a href="#">Beers</a></li>
+                                            <li><a href="#">Ciders</a></li>
+                                            <li><a href="#">Wines</a></li>
+                                            <li><a href="#">Spirits</a></li>
+                                    </ul>
+                                </li>
+                            </div>
+                            <!--<a href="/Links/clients.html">CLIENTS</a> -->
+                            <div class="un_drop">
+                                <li><a href="#">VENDORS</a></li>
+                                <li><a href="owner.php">OWNER</a></li>
+                                <li><a href="about.php">ABOUT US</a></li>
+                                <li><a href="contacts.php">CONTACT US</a></li>
+                            </div>
+                        </ul>
+    </div>
+    </div>
       <div class="body">
             <p>
         Are you new here? Please click
