fe34763d8cc4473a3676ff64274b747a1d44c967
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-08-25 14:07:05 +0200

    edited file

--- Links/nav.php
+++ Links/nav.php
@@ -7,19 +7,35 @@
 <html>
     
     <body>
-        <nav >
         <div class="container">
     <div class="logo">
-        <img src="../pictures/logo.PNG" width="100px" alt="">
+        <img src="../pictures/logo.PNG" width="100px" alt="our logo">
     </div>
     <div class="menu">
-                        <a class="active" href="/index.php">HOME</a>
-                        <a href="Links/store.php">OUR PRODUCTS</a>
-                        <!--<a href="/Links/clients.html">CLIENTS</a> -->
-                        <a href="Links/vendor.php">VENDORS</a>
-                        <a href="Links/owner.php">OWNER</a>
-                        <a href="Links/about.php">ABOUT US</a>
-                        <a href="Links/contacts.php">CONTACT US</a>
+                        <ul>
+                                <li><a class="un_drop" href="/index.php">HOME</a></li>
+                                <div class="drop" >
+                                <li> <a id="products" >OUR PRODUCTS</a>
+                                    <ul id="drop_list">
+                                            <li><a href="#">Water</a></li>
+                                            <li><a href="#">Milk</a></li>
+                                            <li><a href="#">Carbonated Drinks</a></li>
+                                            <li><a href="#">Juice and plent drinks</a></li>
+                                            <li><a href="#">Beers</a></li>
+                                            <li><a href="#">Ciders</a></li>
+                                            <li><a href="#">Wines</a></li>
+                                            <li><a href="#">Spirits</a></li>
+                                    </ul>
+                                </li>
+                            </div>
+                            <!--<a href="/Links/clients.html">CLIENTS</a> -->
+                            <div class="un_drop">
+                                <li><a href="Links/vendor.php">VENDORS</a></li>
+                                <li><a href="Links/owner.php">OWNER</a></li>
+                                <li><a href="Links/about.php">ABOUT US</a></li>
+                                <li><a href="Links/contacts.php">CONTACT US</a></li>
+                            </div>
+                        </ul>
     </div>
     </div>
     </body>
